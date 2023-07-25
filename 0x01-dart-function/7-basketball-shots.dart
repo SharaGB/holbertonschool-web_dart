8ccfd2bd53e78b030d2dafb2bfc18682d4790831
SharaGB <shara.garcia.betancur@gmail.com>
2023-07-25 00:09:50 -0500

    Calculates the total number of points for each team and return the winning

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,25 +1,42 @@
-num getPoints(Map team) {
-  num points = 0;
-  team.forEach((key, value) {
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int pointTeamA = 0;
+  int pointTeamB = 0;
+
+  teamA.forEach((key, value) {
     if (key == 'Free throws') {
-      points += value;
-    } else if (key == '2 pointer') {
-      points += value * 2;
-    } else if (key == '3 pointer') {
-      points += value * 3;
+      pointTeamA = pointTeamA + value;
+    }
+
+    if (key == '2 pointer') {
+      pointTeamA = pointTeamA + (value * 2);
+    }
+
+    if (key == '3 pointer') {
+      pointTeamA = pointTeamA + (value * 3);
     }
   });
-  return points;
-}
 
-num whoWins(Map teamA, Map teamB) {
-  num teamAPoints = getPoints(teamA);
-  num teamBPoints = getPoints(teamB);
-  if (teamAPoints > teamBPoints) {
+  teamB.forEach((key, value) {
+     if (key == 'Free throws') {
+      pointTeamB = pointTeamB + value;
+    }
+
+    if (key == '2 pointer') {
+      pointTeamB = pointTeamB + (value * 2);
+    }
+
+    if (key == '3 pointer') {
+      pointTeamB = pointTeamB + (value * 3);
+    }
+  });
+
+  if (pointTeamA > pointTeamB) {
     return 1;
-  } else if (teamAPoints < teamBPoints) {
-    return 2;
-  } else {
-    return 0;
   }
+
+  if (pointTeamA < pointTeamB) {
+    return 2;
+  }
+
+  return 0;
 }
